5dadc4c222eb7312ee4b4d65661b44baf52f2f28
Rasha Alharhi <rasha.alorabi@gmail.com>
2019-03-18 19:00:48 +0300

    create & fetch channels

---
+++ src/components/ChannelForm.js
@@ -0,0 +1,96 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+
+// Actions
+import * as actionCreators from "../store/actions";
+
+class ChannelForm extends Component {
+  state = {
+    name: "",
+    owner: "",
+    image_url: ""
+  };
+
+  componentWillUnmount() {
+    if (this.props.errors.length) this.props.resetErrors();
+  }
+
+  changeHandler = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  submitHandler = e => {
+    e.preventDefault();
+    this.props.createChannels(this.state, this.props.history);
+  };
+
+  render() {
+    const { name, owner, image_url } = this.state;
+
+    return (
+      <div className="mt-5 p-2 ml-5">
+        <form onSubmit={this.submitHandler}>
+          {!!errors.length && (
+            <div className="alert alert-danger" role="alert">
+              {errors.map(error => (
+                <p key={error}>{error}</p>
+              ))}
+            </div>
+          )}
+          <div className="form-group">
+            <input
+              className="form-control"
+              type="text"
+              placeholder="name"
+              name="name"
+              value={name}
+              onChange={this.changeHandler}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              className="form-control"
+              type="text"
+              placeholder="owner"
+              name="owner"
+              value={owner}
+              // value={this.props.user.username}
+              onChange={this.changeHandler}
+              disabled
+            />
+          </div>
+          <div className="form-group">
+            <input
+              className="form-control"
+              type="text"
+              placeholder="image_url"
+              name="image_url"
+              value={image_url}
+              onChange={this.changeHandler}
+            />
+          </div>
+          <input className="btn btn-primary" type="submit" />
+        </form>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    errors: state.errors.errors,
+    user: state.auth.user
+  };
+};
+
+const mapDispatchToProps = dispatch => ({
+  createChannels: (userData, history) =>
+    dispatch(actionCreators.createChannels(userData, history)), //pass state
+  resetErrors: () => dispatch(actionCreators.resetErrors())
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ChannelForm);
--- src/components/Navigation/SideNav.js
+++ src/components/Navigation/SideNav.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import { connect } from "react-redux";
+import * as actionCreators from "../../store/actions";
 // Fontawesome
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
@@ -14,22 +15,32 @@ import ChannelNavLink from "./ChannelNavLink";
 
 class SideNav extends React.Component {
   state = { collapsed: false };
+  componentDidMount() {
+    this.props.onFetchChannels();
+  }
 
   render() {
-    const channelLinks = [{ name: "all" }].map(channel => (
+    const channelLinks = this.props.channels.map(channel => (
       <ChannelNavLink key={channel.name} channel={channel} />
     ));
     return (
-      <div>
-        <ul className="navbar-nav navbar-sidenav" id="exampleAccordion">
-          <li className="nav-item" data-toggle="tooltip" data-placement="right">
-            <Link className="nav-link heading" to="/createChannel">
-              <span className="nav-link-text mr-2">Channels</span>
-              <FontAwesomeIcon icon={faPlusCircle} />
-            </Link>
-          </li>
-          {channelLinks}
-        </ul>
+      <div className="smooth-scroll">
+        {this.props.user && (
+          <ul className="navbar-nav navbar-sidenav " id="exampleAccordion">
+            <li
+              className="nav-item"
+              data-toggle="tooltip"
+              data-placement="right"
+            >
+              <Link className="nav-link heading" to="/createChannel">
+                <span className="nav-link-text mr-2">Channels</span>
+                <FontAwesomeIcon icon={faPlusCircle} />
+              </Link>
+            </li>
+            {channelLinks}
+          </ul>
+        )}
+
         <ul className="navbar-nav sidenav-toggler">
           <li className="nav-item">
             <span
@@ -51,5 +62,18 @@ class SideNav extends React.Component {
     );
   }
 }
+const mapStateToProps = state => ({
+  user: state.auth.user,
+  channels: state.channels.channels
+});
 
-export default SideNav;
+const mapDispatchToProps = dispatch => {
+  return {
+    onFetchChannels: () => dispatch(actionCreators.fetchChannels())
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(SideNav);
